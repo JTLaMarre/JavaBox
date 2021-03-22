107588cdfbe5a04ca050f9cb2f8d56579b0a164a
isaiah <isotodev@gmail.com>
2021-03-22 17:20:12 -0500

    Made Repo Methods Async

--- GameAPI/Repository/Repo.cs
+++ GameAPI/Repository/Repo.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using GameAPI.Storage;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,55 +18,55 @@ namespace GameAPI.Repository
         /// Gets all Entities
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        public IList<T> Get<T>() where T : class
+        public async Task<List<T>> Get<T>() where T : class
         {
-            return _db.Set<T>().ToList();
+            return await Task.FromResult(_db.Set<T>().ToList());
         }
 
         /// <summary>
         /// Gets Entity based on ID
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        public T Get<T>(object id) where T : class
+        public async Task<T> Get<T>(object id) where T : class
         {
-            return _db.Set<T>().Find(id);
+            return await Task.FromResult(_db.Set<T>().Find(id));
         }
 
         /// <summary>
         /// Inserts an object to a DBSet
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        public void Insert<T>(T obj) where T : class
+        public async void Insert<T>(T obj) where T : class
         {
-            _db.Set<T>().Add(obj);
+            await Task.Run(() =>_db.Set<T>().Add(obj));
         }
 
         /// <summary>
         /// Updates an existing property
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        public void Update<T>(T obj) where T : class
+        public async void Update<T>(T obj) where T : class
         {
-            _db.Set<T>().Attach(obj);
-            _db.Entry(obj).State = EntityState.Modified;
+           await Task.Run(() => _db.Set<T>().Attach(obj));
+           await Task.Run(() =>_db.Entry(obj).State = EntityState.Modified);
         }
 
         /// <summary>
         /// Deletes an existing Entity
         /// </summary>
         /// <typeparam name="T"></typeparam>
-        public void Delete<T>(object id) where T : class
+        public async void Delete<T>(object id) where T : class
         {
-            T existing = _db.Set<T>().Find(id);
-            _db.Set<T>().Remove(existing);
+            T existing = await Task.Run(() => _db.Set<T>().Find(id));
+            await Task.Run(()=>_db.Set<T>().Remove(existing));
         }
 
         /// <summary>
         /// Saves any and all Changes
         /// </summary>
-        public void Save()
+        public async void Save()
         {
-            _db.SaveChanges();
+           await Task.Run(()=>_db.SaveChanges());
         }
     }
 }
--- GameAPI/Service/Controllers/GameController.cs
+++ GameAPI/Service/Controllers/GameController.cs
@@ -1,9 +1,31 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GameAPI.Domain.Models;
+using GameAPI.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GameAPI.Service.Controllers
 {
-    public class GameController : Controller
+    [ApiController]
+    [Route("rest/game")]
+    public class GameController : ControllerBase
     {
+        private readonly Repo _repo;
+        public GameController(Repo _repo)
+        {
+            this._repo = _repo;
+        }
         
+        /// <summary>
+        /// Get all user accounts available
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Game>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _repo.Get<Game>());
+        }
     }
 }
