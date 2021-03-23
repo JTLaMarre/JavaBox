63856c68cb7098eb7ccd75576e0ae4117f1a1ec6
Jacob LaMarre <lordaufchaos@yahoo.com>
2021-03-23 13:01:49 -0400

    working on routing

--- HostFrontEnd/src/app/app-routing.module.ts
+++ HostFrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HostComponent } from '../app/components/host/host.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { PlayerComponent } from './components/player/player.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LandingComponent},
+  {path:'host',component:HostComponent},
+  {path:'player',component:PlayerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
